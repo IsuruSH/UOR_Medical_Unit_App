8f7f4fdd7714d661836d2d225f92f005ff49d6ba
Aathifmhd <aathifmhd2000@gmail.com>
2024-06-30 02:12:09 +0530

    page created

---
+++ frontend/app/pages/doctor/layout.tsx
@@ -0,0 +1,21 @@
+import Header from '@/components/doctor/header';
+import { Metadata } from 'next';
+import React from 'react'
+
+export const metadata: Metadata = {
+    title: "doctor",
+    description: "UOR Medical doctor",
+  };
+
+const StudentLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <div className='pt-5 w-11/12 m-auto'>
+      <Header/>
+        <div>
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default StudentLayout
---
+++ frontend/app/pages/doctor/overallsummary/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page3 = () => {
+  return (
+    <div>
+      This is Doctor's overall summary page.
+    </div>
+  )
+}
+
+export default page3

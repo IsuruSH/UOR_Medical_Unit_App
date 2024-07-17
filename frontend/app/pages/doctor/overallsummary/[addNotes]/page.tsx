4a9a82dbb3bf84039c4ab3efd8a5705ac90b65c9
Janith Ranasinghe <janithraditha@gmail.com>
2024-07-18 00:11:41 +0530

    create add button using dynamic navigation for unique stident

--- frontend/app/pages/doctor/overallsummary/[addNotes]/page.tsx
+++ frontend/app/pages/doctor/overallsummary/[addNotes]/page.tsx
@@ -1,43 +1,64 @@
-import React from "react";
+"use client";
+import React, {FC} from "react";
+import Link from 'next/link';
+import patientDetails from "../../../../../data/doctor/patientDetails.json";
 
-function Notes() {
-    return (
-        <div className="w-full p-5">
-            <div className="w-full mb-5">
-                <button className="bg-gray font-bold text-[#386641] px-8 rounded-sm">Back</button>
-            </div>
-            <div className="w-full flex justify-center items-center">
-                <div className="w-full md:w-1/2">
-                    <form className="w-full grid grid-cols-1 gap-5">
-                        <div className="w-full grid grid-cols-2">
-                            <label>Appointment ID :</label>
-                            <input type="text"
-                                   className="w-full border-2 border-gray rounded-md focus:outline-none bg-transparent"/>
-                        </div>
-                        <div className="w-full grid grid-cols-2">
-                            <label>Name :</label>
-                            <input type="text"
-                                   className="w-full border-2 border-gray rounded-md focus:outline-none bg-transparent"/>
-                        </div>
-                        <div className="w-full grid grid-cols-2">
-                            <label>Sc Number :</label>
-                            <input type="text"
-                                   className="w-full border-2 border-gray rounded-md focus:outline-none bg-transparent"/>
-                        </div>
-                        <div className="w-full grid grid-cols-2">
-                            <label>Notes :</label>
-                            <textarea name="note" id="" placeholder="Enter notes"
-                                      className="w-full h-32 border-2 border-gray rounded-md focus:outline-none bg-transparent"></textarea>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div className="w-full flex justify-center gap-10 mt-5">
-                <button className="bg-[#00B69B] font-bold px-6 rounded-sm">clear</button>
-                <button className="bg-[#979797] font-bold text-[#6226EF] px-6 rounded-sm">submit</button>
-            </div>
-        </div>
-    );
+interface pageProps {
+    params: {
+        addNotes: string;
+    }
 }
 
-export default Notes;
+const Page: FC<pageProps> = ({params}) => {
+
+    for (const item of patientDetails) {
+        if (params.addNotes == item.id) {
+            return (
+                <div className="w-full p-5">
+                    <div className="w-full mb-5">
+                        <Link href={'/pages/doctor/overallsummary'}
+                              className="bg-gray font-bold text-[#386641] px-8 rounded-sm text-lg"
+                        >
+                            Back
+                        </Link>
+                    </div>
+                    <div className="w-full flex justify-center items-center">
+                        <div className="w-full md:w-1/2">
+                            <form className="w-full grid grid-cols-1 gap-5">
+                                <div className="w-full grid grid-cols-2">
+                                    <label>Appointment ID :</label>
+                                    <div className="w-full border-2 border-gray rounded-md bg-transparent p-1">
+                                        0001
+                                    </div>
+                                </div>
+                                <div className="w-full grid grid-cols-2">
+                                    <label>Name :</label>
+                                    <div className="w-full border-2 border-gray rounded-md bg-transparent p-1">
+                                        {item.name}
+                                    </div>
+                                </div>
+                                <div className="w-full grid grid-cols-2">
+                                    <label>Sc Number :</label>
+                                    <div className="w-full border-2 border-gray rounded-md bg-transparent p-1">
+                                        {item.id}
+                                    </div>
+                                </div>
+                                <div className="w-full grid grid-cols-2">
+                                    <label>Notes :</label>
+                                    <textarea name="note" id="" placeholder="Enter notes"
+                                              className="w-full h-32 border-2 border-gray rounded-md focus:outline-none bg-transparent"></textarea>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="w-full flex justify-center gap-10 mt-5">
+                        <button className="bg-[#00B69B] font-bold px-6 rounded-sm">clear</button>
+                        <button className="bg-[#979797] font-bold text-[#6226EF] px-6 rounded-sm">submit</button>
+                    </div>
+                </div>
+            );
+        }
+    }
+}
+
+export default Page;
--- frontend/app/pages/doctor/overallsummary/page.tsx
+++ frontend/app/pages/doctor/overallsummary/page.tsx
@@ -9,42 +9,42 @@ const Page: React.FC = () => {
     const [search, setSearch] = useState("");
 
     return (
-            <div className="overflow-auto h-300 xl:h-500 2xl:h-800 shadow-md rounded-md">
-                <table className="table-inner w-full text-xs lg:text-base 2xl:text-xl table-outer">
-                    <thead className='bg-white-3 sticky top-0 shadow-md'>
-                    <tr>
-                        <th className='p-3 text-center'>APPOINTMENT NO</th>
-                        <th className='p-3 text-center'>Name</th>
-                        <th className='p-3 text-center'>Student-ID</th>
-                        <th className='p-3 text-center'>Notes</th>
-                        <th className='p-3 text-center'>Consult Status</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        patientDetails.map((patient, index) => (
-                            <tr key={index}>
-                                <td className='p-3 text-center'>
-                                    <a href="" className="hover:text-light-green">{index + 1}</a>
-                                </td>
-                                <td className='p-3 text-center'>{patient.name}</td>
-                                <td className='p-3 text-center'>{patient.id}</td>
-                                <td className='p-3 text-center'>
-                                    <Link href={`/pages/doctor/overallsummary/${patient.id}`}
-                                        className="text-light-green bg-white-2 px-5 rounded-xl"
-                                    >
-                                        Add
-                                    </Link>
-                                </td>
-                                <td className='p-3 text-center'>
+        <div className="overflow-auto h-300 xl:h-500 2xl:h-800 shadow-md rounded-md">
+            <table className="table-inner w-full text-xs lg:text-base 2xl:text-xl table-outer">
+                <thead className='bg-white-3 sticky top-0 shadow-md'>
+                <tr>
+                    <th className='p-3 text-center'>APPOINTMENT NO</th>
+                    <th className='p-3 text-center'>Name</th>
+                    <th className='p-3 text-center'>Student-ID</th>
+                    <th className='p-3 text-center'>Notes</th>
+                    <th className='p-3 text-center'>Consult Status</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    patientDetails.map((patient, index) => (
+                        <tr key={index}>
+                            <td className='p-3 text-center'>
+                                <a href="" className="hover:text-light-green">{index + 1}</a>
+                            </td>
+                            <td className='p-3 text-center'>{patient.name}</td>
+                            <td className='p-3 text-center'>{patient.id}</td>
+                            <td className='p-3 text-center'>
+                                <Link href={`/pages/doctor/overallsummary/${patient.id}`}
+                                      className="text-light-green bg-white-2 px-5 rounded-xl"
+                                >
+                                    Add
+                                </Link>
+                            </td>
+                            <td className='p-3 text-center'>
                                 <button className="text-purple bg-white-2 px-5 rounded-xl">Processing</button>
-                                </td>
-                            </tr>
-                        ))
-                    }
-                    </tbody>
-                </table>
-            </div>
+                            </td>
+                        </tr>
+                    ))
+                }
+                </tbody>
+            </table>
+        </div>
     )
 }
 
